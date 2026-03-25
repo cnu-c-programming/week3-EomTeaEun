22a3dff9f5bdafabe0248ee987b5d2edc419f97a
EomTeaEun <xodms3320@gmail.com>
2026-03-25 17:43:44 +0900

    update codes

--- code/2-2.c
+++ code/2-2.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
+void swap(int *a, int *b) {
+   int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 
+int main() {
+    int x=100, y=200;
+    printf("x = %d, y = %d\n", x, y);
+    
+    swap(&x, &y);
+    printf("x = %d, y = %d\n", x, y);
     return 0;
 }
+
--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+void my_sum(char type, int count, ...) {
+    va_list args;
+    va_start(args, count); 
 
+    int total_sum = 0;
+
+    for (int i = 0; i < count; i++) {
+        if (type == 'S') {
+            char* s = va_arg(args, char*);
+            printf("%s ", s);
+        } 
+        else if (type == 'C') {
+            char c = (char)va_arg(args, int);
+            printf("%c", c);
+        } 
+        else if (type == 'D') {
+            total_sum += va_arg(args, int);
+        }
+    }
+
+    if (type == 'D') {
+        printf("%d", total_sum);
+    }
+    printf("\n"); 
+
+    va_end(args); 
+}
+
+int main() {
+    my_sum('S',2,"Hello","World");
+    my_sum('C',3,'C','N','U');
+    my_sum('D',4,10,20,30,40);
     return 0;
 }
